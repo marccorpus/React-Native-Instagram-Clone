d192f5c6383b6d584eb27c1346fe1e6f4072213b
marccorpus <marccorpus.dev@gmail.com>
2024-06-05 13:12:22 +0800

    create app themes

---
+++ src/themes/colors.ts
@@ -0,0 +1,8 @@
+export default {
+  primary: '#405DE6',
+
+  black: '#000000',
+  white: '#ffffff',
+  gray: '#666666',
+  lightGray: '#cccccc',
+};
---
+++ src/themes/fonts.ts
@@ -0,0 +1,15 @@
+const size = {
+  sm: 12,
+  normal: 14,
+  md: 16,
+  lg: 20,
+  xl: 24,
+};
+
+const weight = {
+  light: '200',
+  normal: 'normal',
+  bold: '700',
+};
+
+export {size, weight};
