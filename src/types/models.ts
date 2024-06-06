40823064b4cd2d0afc0f3d30125ae95aa0ea323e
marccorpus <marccorpus.dev@gmail.com>
2024-06-06 13:21:04 +0800

    sample posts data

---
+++ src/types/models.ts
@@ -0,0 +1,22 @@
+export interface IUser {
+  id: number;
+  username: string;
+  image?: string;
+}
+
+export interface IComment {
+  id: number;
+  comment: string;
+  user: IUser;
+}
+
+export interface IPost {
+  id: number;
+  image?: string;
+  description: string;
+  nofComments: number;
+  nofLikes: number;
+  comments: IComment[];
+  user: IUser;
+  createdAt: string;
+}
